d9dd52ceea6d2a03a41d9cdfbaa0a8235b6f17f8
plmn65410 <69616757+plmn65410@users.noreply.github.com>
2020-12-22 11:20:16 +0700

    Add files via upload

---
+++ Week5/Jave101.js
@@ -0,0 +1,36 @@
+function Function3(){
+    let x = document.getElementById("myBtn3").value
+    // document.getElementById("images").src="green.jpg";
+    document.getElementById("images").src = x ;
+}
+
+function Function4(){
+    let x = document.getElementById("myBtn4").value
+    // document.getElementById("images").src="blue.jpg";
+    document.getElementById("images").src = x ;
+}
+
+function Function5(){
+    let x = document.getElementById("myBtn5").value
+    // document.getElementById("images").src="red.jpg";
+    document.getElementById("images").src = x ;
+}
+
+function Funtion6(){
+    document.getElementById("text").style.color="green";
+}
+
+function Funtion7(){
+    document.getElementById("text").style.color="blue";
+
+function Funtion8(){
+    document.getElementById("text").style.color="brown";
+}
+
+function Funtion9(){
+    document.getElementById("hideDisplay").style.color="none";
+}
+
+function Funtion10(){
+    document.getElementById("hideDisplay").style.color="block";
+}
